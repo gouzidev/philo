fa056e9222cc6bc153215e04580b64d34ce9e534
gouzidev <gouzi.dev@gmail.com>
2024-05-19 19:56:22 +0100

    works ...?

--- safe.c
+++ safe.c
@@ -11,6 +11,7 @@ long    get_done(t_data *data)
     UNLOCK(&data->done_mutex);
     return (0);
 }
+
 long get_ready_threads(t_data *data)
 {
     long ready_threads;
@@ -37,3 +38,16 @@ long    get_last_ate(t_philo *philo)
     return (last_ate);
 }
 
+void set_done(t_data *data, long new_done)
+{
+    LOCK(&data->done_mutex);
+    data->done = new_done;
+    UNLOCK(&data->done_mutex);
+}
+
+void set_last_ate(t_philo *philo, long new_last_ate)
+{
+    LOCK(&philo->last_ate_mutex);
+    philo->last_ate = new_last_ate;
+    UNLOCK(&philo->last_ate_mutex);
+}
--- safe1.c
+++ safe1.c
@@ -1,15 +1,2 @@
 #include "philo.h"
 
-void set_done(t_data *data, long new_done)
-{
-    LOCK(&data->done_mutex);
-    data->done = new_done;
-    UNLOCK(&data->done_mutex);
-}
-
-void set_last_ate(t_philo *philo, long new_last_ate)
-{
-    LOCK(&philo->last_ate_mutex);
-    philo->last_ate = new_last_ate;
-    UNLOCK(&philo->last_ate_mutex);
-}
